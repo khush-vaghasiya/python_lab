92b2204f3fe9db1cd9cee14e829d525863693a03
khush-vaghasiya <khushvaghasiya158@gmail.com>
2026-01-05 11:15:36 +0530

    Add files via upload

---
+++ datatype_one.py
@@ -0,0 +1,40 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x="hello world"
+print(x)
+hello world
+x=3
+print(x)
+3
+x=true
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    x=true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+x=true
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    x=true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+x="true"
+y="false"
+print(x)
+true
+x=["apple","banana","cherry"]
+print(x)
+['apple', 'banana', 'cherry']
+x=["apple","banana","cherry"]
+print(x)
+['apple', 'banana', 'cherry']
+x=["name","john","age",40]
+print(x)
+['name', 'john', 'age', 40]
+x="None"
+print(x)
+None
+x="3j"
+print(x)
+3j
+x="55.5"
+print(x)
+55.5
