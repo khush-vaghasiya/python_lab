2a566efdd1f178c10c6196e40281a2997ecc3bb2
khush-vaghasiya <khushvaghasiya158@gmail.com>
2026-02-11 08:38:16 +0530

    Add files via upload

---
+++ cons.py
@@ -0,0 +1,38 @@
+#voting eligible program:-
+"""age=int(input("Enter your age:"))
+if age>=18:
+    print("yes you are aligible.")
+else:
+    print("you are not aligible.")"""
+    
+#licence eligible program:-
+"""age =int(input("Enter your age:"))
+if age>=18:
+    print("your are eligible for licence")
+else: 
+    print("yoy are not eligible")"""
+    
+#positive and nagetive program:-
+"""num =int(input("Enter your lucky number:"))
+if num>0:
+    print("wow! your lucky number is positive...")
+elif num<0:
+    print("ohhhh! your lucky number is nagetive...")
+else:
+    print("hurrah! your lucky number is mindi...")"""
+    
+#odd and even program:
+"""taher =int(input("Enter your number:"))
+if (taher % 2) == 0:
+    print("your number is a even...")
+else:
+    print("your number is odd...")"""
+    
+#mark's program:-
+aryan = int(input("enter your mark :-"))
+if aryan>80:
+    print("you get a grade...")
+elif aryan>=70:
+    print("you get b grade...")
+else:
+    print("you only pass...")    
