fbc750b3cb1ad31ade92c51a3930422c60a2db54
khush-vaghasiya <khushvaghasiya158@gmail.com>
2026-02-16 11:32:21 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,48 @@
+# print number from 1to 10
+'''for T in range (1,11):
+    print(T)'''
+
+    
+# print even numbers from 1 to 20 \
+'''for Z in range (1,21):
+    if Z % 2 == 0:
+        print(Z)'''
+
+  
+#print odd number from 1 to 15
+'''for P in range (1,16):
+    if P % 2 != 0:
+        print(P)   '''  
+        
+
+#print table of 1 to 5
+'''for B in range(1,6):
+    print(B) '''
+    
+    
+#print message 3 time 
+'''for D in range(3):
+    print("hello kya hal chal")  '''                   
+    
+# print table of 5
+'''for Y in range (1,11):
+    print("5X", Y ,"=",5*Y)  ''' 
+    
+    
+#print characters of a string 
+name= "aryan"
+'''for letter in name :
+    print(letter)  '''   
+    
+    
+# # sum of number form 1 to 5 total=0
+total=0
+'''for U in range (1,6):
+    total = total + U
+print("sum is :",total) '''
+
+        
+#print list elements
+numbers=[10,20,30,40]
+for O in numbers :
+    print(O)  
