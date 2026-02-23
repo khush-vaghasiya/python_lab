51e962ed7b69e256ac6a87e9ae4f9497bba25c92
khush-vaghasiya <khushvaghasiya158@gmail.com>
2026-02-23 11:22:40 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,26 @@
+'''i=1
+while i<=5:
+   print(i)
+   i=i+1'''
+   
+#print number from 1 to 10
+'''p=1
+while p<=10:
+    print(p)
+    p=p+1'''
+    
+#sum of first n natural number
+'''z=int(input("enter z:"))
+p=1
+t=0
+while p<=z:
+    t=t+p
+    p=p+1
+    print("sum=",t)    '''
+
+#table of a number
+num=int(input("enter number:"))
+k=1
+while k<=10:
+    print(num,"x",k,"=",num*k)
+    k=k+1
