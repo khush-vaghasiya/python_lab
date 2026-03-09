7a3969ae511f0263dbad389659062d64e3894564
khush-vaghasiya <khushvaghasiya158@gmail.com>
2026-03-09 11:36:32 +0530

    Add files via upload

---
+++ array1.py
@@ -0,0 +1,45 @@
+# len () number of element
+'''from import array
+arr = array('i',[10,20,30,40,50])
+print(len(arr))'''
+
+# append(x)- add element at end
+'''from array import array
+arr=array('i',[10,20,30])
+arr.append(40)
+print(arr)'''
+
+# insert(pos,x)-insert at position
+'''from array import array
+arr=array('i',[10,20,40])
+arr.insert(2,30)
+print(arr)'''
+
+#remove (x)-remove first occurrence
+'''from array import array
+arr=array('i',[10,20,30,20,40])
+arr.remove(20)
+print(arr)'''
+
+#pop-remove and return last element
+'''from array import array
+arr=array('i',[10,20,30,40])
+x=arr.pop()
+print("removed",x)
+print(array)'''
+
+#index(x)-find index of element
+'''from array import array
+arr=array('i',[10,20,30,40])
+print(arr.index(30))'''
+
+#count(x)-count occurrences
+'''from array import array 
+arr=array('i',[10,20,30,20,40])
+print(arr.count(20))'''
+
+#reverse()-reverse array
+'''from array import array
+arr=array('i',[10,20,30,40])
+arr.reverse()
+print(arr)'''
