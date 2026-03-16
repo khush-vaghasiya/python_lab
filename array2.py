3045b8aee0bf02184a870a9f07177b66cb2730c9
khush-vaghasiya <khushvaghasiya158@gmail.com>
2026-03-16 11:39:26 +0530

    Add files via upload

---
+++ array2.py
@@ -0,0 +1,25 @@
+# indexing in array#
+#positive indexing
+'''from array import array 
+arr = array ('i',[10,20,30,40,50])
+print(arr[0])
+print(arr[1])
+print(arr[4])'''
+
+#negative indexing
+'''from array import array
+arr = array('i',[10,20,30,40,50])
+print(arr[-1])
+print(arr[-2])
+print(arr[-5])'''
+
+#modifying element using index
+'''from array import array
+arr=array('i',[10,20,30,40,50])
+arr[2]=35
+print(arr)'''
+
+#index array
+'''from array import array
+arr=array('i',[10,20,30])
+print(arr[5])'''
